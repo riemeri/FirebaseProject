a6fec843af40d815683df093ba1ad1029a99dab7
riemeri <32551363+riemeri@users.noreply.github.com>
2019-01-10 01:26:13 -0800

    removed excess file
    also added an error alert for logouts

--- public/details.js
+++ public/details.js
@@ -3,7 +3,7 @@ logoutBtn.addEventListener('click', (ev) => {
 	firebase.auth().signOut().then(() => {
 		window.location.href = "https://fir-notes-2eb81.firebaseapp.com/";
 	}).catch(err => {
-		//TODO: Handle errors here.
+		alert('Error: ' + err.message);
 		console.log(error);
 	});
 }, false);
